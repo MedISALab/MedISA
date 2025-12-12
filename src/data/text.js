079feb34ee3d2570e1db16a5ad0096cd47ac02e5
apaulbnk <51493934+apaulbnk@users.noreply.github.com>
2025-12-12 14:19:52 +0530

    Revise texts for MedISA Lab and research overview
    Updated the texts export to reflect changes in titles and descriptions, emphasizing AI solutions for medical image synthesis and analysis.

--- src/data/text.js
+++ src/data/text.js
@@ -1,10 +1,11 @@
 export const texts = {
   banner: {
-    title: "The MedISA LAB",
-    description: "<strong>MedISA</strong> stands for <strong>Medical Image Synthesis and Analysis</strong> — a research area dedicated to creating and interpreting visual data using advanced machine learning and deep learning techniques."
+    title: "The MedISA Lab",
+    // description: "<strong>The MedISA</strong>Lab<strong> at IIT Jodhpur focusses on medical image synthesis and analysis</strong> problems using advanced AI techniques for affordable healthcare."
   },
   highlights: {
     title: "RESEARCH OVERVIEW",
-    description: "Our lab focuses on developing innovative deep learning solutions for medical imaging problems. Our research addresses key problems in areas such as disease detection and diagnosis. Currently, significant work is being done in medical image classification, segmentation, reconstruction, super resolution, synthetic data generation, etc. Our work also addresses challenges related to learning from distributed data, vision language modeling and reinforcement learning. Our goal is to develop deep learning solutions that are robust and effective in any real-world clinical settings.",
+    description: "Our lab focuses on developing innovative AI solutions for medical image sysnthesis and analysis problems to facilitate affordable healthcare. Our research addresses key problems in areas such as disease diagnosis, segmentation, reconstruction, super resolution, synthetic data generation, etc. Our work also addresses challenges related to learning from distributed data, vision language modeling and reinforcement learning. Our goal is to develop deep learning solutions that are robust and effective in any real-world clinical settings.",
   }
 };
+
