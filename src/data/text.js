f133778b68fa64cebc002304b835c2d53cffb0b7
MedISALab <medisa.iitj@gmail.com>
2025-12-12 14:06:56 +0530

    few tweaks

--- src/data/text.js
+++ src/data/text.js
@@ -4,7 +4,7 @@ export const texts = {
     description: "<strong>MedISA</strong> stands for <strong>Medical Image Synthesis and Analysis</strong> — a research area dedicated to creating and interpreting visual data using advanced machine learning and deep learning techniques."
   },
   highlights: {
-    heading: "RESEARCH OVERVIEW",
-    content: "Our lab focuses on developing innovative deep learning solutions for medical imaging problems. Our research addresses key problems in areas such as disease detection and diagnosis. Currently, significant work is being done in medical image classification, segmentation, reconstruction, super resolution, synthetic data generation, etc. Our work also addresses challenges related to learning from distributed data, vision language modeling and reinforcement learning. Our goal is to develop deep learning solutions that are robust and effective in any real-world clinical settings.",
+    title: "RESEARCH OVERVIEW",
+    description: "Our lab focuses on developing innovative deep learning solutions for medical imaging problems. Our research addresses key problems in areas such as disease detection and diagnosis. Currently, significant work is being done in medical image classification, segmentation, reconstruction, super resolution, synthetic data generation, etc. Our work also addresses challenges related to learning from distributed data, vision language modeling and reinforcement learning. Our goal is to develop deep learning solutions that are robust and effective in any real-world clinical settings.",
   }
 };
