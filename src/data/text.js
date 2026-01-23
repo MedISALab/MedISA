9617ccbf763972db8df076fd9bbecfed59b48d63
apaulbnk <51493934+apaulbnk@users.noreply.github.com>
2026-01-23 20:08:14 +0530

    Revise texts export for MedISA Lab details
    Updated the title and description in the texts export for clarity and conciseness.

--- src/data/text.js
+++ src/data/text.js
@@ -1,11 +1,11 @@
 export const texts = {
   banner: {
-    title: "The Medical Image Synthesis & Aanalysis Lab at IIT Jodhpur",
+    title: "MedISA Lab @ IIT Jodhpur",
     description: "" //"<strong>The MedISA</strong>Lab<strong> at IIT Jodhpur focusses on medical image synthesis and analysis</strong> problems using advanced AI techniques for affordable healthcare."
   },
   highlights: {
     title: "RESEARCH OVERVIEW",
-    description: "Our lab focuses on developing innovative AI solutions for medical image synthesis and analysis problems to facilitate affordable healthcare. Our research addresses key problems in areas such as disease diagnosis, segmentation, reconstruction, super resolution, synthetic data generation, etc. Our work also addresses challenges related to learning from distributed data, vision language modeling and reinforcement learning. Our goal is to develop deep learning solutions that are robust and effective in any real-world clinical settings.",
+    description: "The MedISA Lab (Medical Image Synthesis & Analysis Lab) focuses on developing innovative AI solutions for medical image synthesis and analysis problems to facilitate affordable healthcare. Our research addresses key problems in areas such as disease diagnosis, segmentation, reconstruction, super resolution, synthetic data generation, etc. Our work also addresses challenges related to learning from distributed data, vision language modeling and reinforcement learning. Our goal is to develop deep learning solutions that are robust and effective in any real-world clinical settings.",
   }
 };
 
