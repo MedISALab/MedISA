d023f359422c67a0871fdc8776b54476062103f6
apaulbnk <51493934+apaulbnk@users.noreply.github.com>
2026-01-23 20:11:58 +0530

    Update text.js

--- src/data/text.js
+++ src/data/text.js
@@ -1,6 +1,6 @@
 export const texts = {
   banner: {
-    title: "MedISA Lab @ IIT Jodhpur",
+    title: "MedISA Lab \\@ IIT Jodhpur",
     description: "" //"<strong>The MedISA</strong>Lab<strong> at IIT Jodhpur focusses on medical image synthesis and analysis</strong> problems using advanced AI techniques for affordable healthcare."
   },
   highlights: {
@@ -8,6 +8,7 @@ export const texts = {
     description: "The MedISA Lab (Medical Image Synthesis & Analysis Lab) focuses on developing innovative AI solutions for medical image synthesis and analysis problems to facilitate affordable healthcare. Our research addresses key problems in areas such as disease diagnosis, segmentation, reconstruction, super resolution, synthetic data generation, etc. Our work also addresses challenges related to learning from distributed data, vision language modeling and reinforcement learning. Our goal is to develop deep learning solutions that are robust and effective in any real-world clinical settings.",
   }
 };
+
 
 
 
